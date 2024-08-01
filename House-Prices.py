0687c1adf23d116f968147c25dabf40b0574645a
mrmacaroni49 <105277683+mrmacaroni49@users.noreply.github.com>
2024-08-01 19:30:53 +0300

    Changed the model

--- House-Prices.py
+++ House-Prices.py
@@ -1,34 +1,39 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-import categorical_variables_2 as CAT
+import numpy as np
+import matplotlib.pyplot as plt
+from catboost import CatBoostRegressor
+import xgboost as xgb
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_absolute_error
-
-# Read the data
-X_full = pd.read_csv(r'C:\Users\adam\PycharmProjects\pythonProject.py\Data\train.csv', index_col='Id')
-X_test_full = pd.read_csv(r'C:\Users\adam\PycharmProjects\pythonProject.py\Data\test.csv', index_col='Id')
-
-# Remove rows with missing target, separate target from predictors
-X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
-y = X_full.SalePrice
-X_full.drop(['SalePrice'], axis=1, inplace=True)
-
-# Break off validation set from training data
-X_train, X_valid, y_train, y_valid = train_test_split(X_full, y,
-                                                                train_size=0.8, test_size=0.2,
-                                                                 random_state=0)
-
-from xgboost import XGBRegressor
+from Scripts import categorical_variables_2 as CAV
+from Scripts import features as FT
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import mean_absolute_error
 
-model = XGBRegressor()
+#Load the Data
+df = pd.read_csv(r'C:\Users\adam\Documents\GitHub\House-Prices-Competition\Data\train.csv', index_col='Id')
+df_test = pd.read_csv(r'C:\Users\adam\Documents\GitHub\House-Prices-Competition\Data\test.csv', index_col='Id')
 
+#Drop the target variable
+df.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = df.SalePrice
+df.drop(['SalePrice'], axis=1, inplace=True)
 
-my_pipeline = Pipeline(steps=[('preprocessor', CAT.Cat_var(X_train, X_valid)),
-                              ('model', model)
-                             ])
+#Split the Data
+from sklearn.model_selection import train_test_split
+X_train, X_valid, y_train, y_valid = train_test_split(df, y, test_size = 0.3, random_state = 0)
 
-my_pipeline.fit(X_train, y_train)
+#Create and Train the model
+model = CatBoostRegressor(learning_rate=0.13)
 
-my_pred = my_pipeline.predict(X_valid)
-print(mean_absolute_error(y_valid, my_pred))
+clf = Pipeline(steps=[('preprocessor', CAV.Cat_var(X_train, X_valid)), ('model', model)])
+clf.fit(X_train, y_train)
+
+#Make the predictions
+pred = clf.predict(X_valid)
+pred_test = clf.predict(FT.XT(X_train, df_test))
+
+print(mean_absolute_error(y_valid, pred))
+
+#Create the submission File
+output = pd.DataFrame({'Id': FT.XT(X_train, df_test).index, 'SalePrice': pred_test})
+output.to_csv("File.csv", index=False)
